8dc68375ac8068ef11d16efe276e072c7806b4b9
hotpad100c <46704388+hotpad100c@users.noreply.github.com>
2025-10-23 13:16:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import markov
-Ryan100C=markov.model()
+Ryan100C=markov.🥵()
 with open("Ryan100C_datas.txt",encoding='utf-8') as f:
     Ryan100C.train(f.readlines())
 print("请欣赏马尔可夫梦话十句：")
 for i in range(10):
+
     Ryan100C.run()
